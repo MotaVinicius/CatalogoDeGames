fdeecf66ddd244915e9269befc3a31d9fad2dd7a
Vinicius Mota <motavinicius@outlook.com>
2023-10-30 00:19:43 -0300

    att dependencies

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express()
 app.use(express.json())
-const port = 3000
+const port = process.env.PORT || 3000 ;
 
 mongoose.connect('mongodb+srv://motavinicius:Aguia999*@catalogogames-api.1b1fxq3.mongodb.net/?retryWrites=true&w=majority');
 const Game = mongoose.model('Game', {
